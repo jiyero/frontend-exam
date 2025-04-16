a764ca55f648fc4b70c10f999e5b2b3adc231b51
jiyero <jihro_abendano@dlsu.edu.ph>
2025-04-16 14:39:00 +0800

    Get users from API and show in table

--- src/views/users/index.js
+++ src/views/users/index.js
@@ -1,7 +1,25 @@
-import React from 'react';
-import {Container, Table, Button} from 'reactstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Table, Button } from 'reactstrap';
 
 function Index() {
+
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		const fetchUsers = async () => {
+			try {
+				const response = await fetch('https://reqres.in/api/users');
+				const data = await response.json();
+				setUsers(data.data);
+			} catch (error) {
+				console.error('Failed to fetch users:', error);
+			}
+		};
+
+		fetchUsers();
+	}, []);
+
+
 	return (
 		<Container>
 			<div className='mt-3 text-right'>
@@ -21,30 +39,21 @@ function Index() {
 				</thead>
 
 				<tbody>
-					<tr>
-						<th scope='row'>1</th>
-						<td />
-						<td />
-						<td>Mark</td>
-						<td>Otto</td>
-						<td />
-					</tr>
-					<tr>
-						<th scope='row'>2</th>
-						<td />
-						<td />
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td />
-					</tr>
-					<tr>
-						<th scope='row'>3</th>
-						<td />
-						<td />
-						<td>Larry</td>
-						<td>the Bird</td>
-						<td />
-					</tr>
+
+					{users.map((user) => (
+						<tr key={user.id}>
+							<th scope='row'>{user.id}</th>
+							<td>
+								<img src={user.avatar}></img>
+							</td>
+							<td>{user.email}</td>
+							<td>{user.first_name}</td>
+							<td>{user.last_name}</td>
+							<button>Edit</button>
+							<button>Delete</button>
+						</tr>
+					))}
+
 				</tbody>
 			</Table>
 		</Container>
